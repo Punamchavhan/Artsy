511fc0077df642f3f6dd83d45fc0d91850af297a
Punam Chavhan <punamchavhan0509@gmail.com>
2023-07-20 23:26:58 +0530

    new files

--- artsy/index.js
+++ artsy/index.js
@@ -1,5 +1,29 @@
+import { Configuration, Configuration, OpenAIApi } from "openai";
+import { process } from './env'
 
 
 
 const outputImg= document.getElementById('output-img')
+
+const Configuration =new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(Configuration);
+document.getElementById("submit-btn").addEventListener("click", () => {
+    const prompt =  document.getElementById("instruction").value;
+    generateImage(prompt);
+
+})
+
+async function generateImage(prompt) {
+    const response = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: '256x256',
+        response_format: 'b64_json'
+    })
+
+    outputImg.innerHTML = '<img src="data:image/png;base64,${response.data.data[0].b64_json}">'
+}
 
